7e2957b86fe732bb839265c6e29ae7f4bbd74263
21chinton <35405722+21chinton@users.noreply.github.com>
2018-02-12 20:15:55 -0500

    Revert "SweepCylinder"
    This reverts commit 140ad2a9c3319b655a44e8ceaf060657f1abe390.

--- src/org/usfirst/frc/team6484/robot/subsystems/SweepMotorSubSystem.java
+++ src/org/usfirst/frc/team6484/robot/subsystems/SweepMotorSubSystem.java
@@ -4,22 +4,20 @@ import org.usfirst.frc.team6484.robot.commands.SweepMotorCommand;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
+//import edu.wpi
 import org.usfirst.frc.team6484.robot.RobotMap;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 /**
  *
  */ 
 public class SweepMotorSubSystem extends Subsystem {
 
-	private static DoubleSolenoid sweepCylinder;
     Spark leftIntake;
     Spark rightIntake;
     DigitalInput limitButton;
     
     public SweepMotorSubSystem()
     {
-    	sweepCylinder = new DoubleSolenoid(RobotMap.sweepIn, RobotMap.sweepOut);
     	leftIntake = new Spark(RobotMap.leftSweeperMotor);
     	rightIntake = new Spark(RobotMap.rightSweeperMotor);
     	limitButton = new DigitalInput(2);
@@ -49,12 +47,5 @@ public class SweepMotorSubSystem extends Subsystem {
         setDefaultCommand(new SweepMotorCommand());
     }
     
-    //SweepCylinder   V
-    public void openSweep() {
-    	sweepCylinder.set(DoubleSolenoid.Value.kForward);
-    }
-    public void closeSweep() {
-    	sweepCylinder.set(DoubleSolenoid.Value.kReverse);
-    }
 }
 
